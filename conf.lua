3e0dc8b80bc4f6ebe7455164fc034e7e04e90617
omar-ea <126994619+WalidOEa@users.noreply.github.com>
2025-02-17 19:15:14 +0000

    Updated conf

--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
-	t.window.width = 900
-	t.window.width = 1400
-	t.window.title = "GAME OF LIFE..."
+	t.window.height = 1080
+	t.window.width = 1920
+	t.window.title = "About"
 end
